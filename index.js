1f6752c2d5ca4c178665c5d006e83779a6e8a534
Jaber <jaberamin018@gmail.com>
2024-09-02 19:38:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+1. 
+let carName = "Volvo";
+
+2.
+let firstName = "John", lastsName = "Doe", age = 35;
+
+3.
+// x = 10;
+// y = 5;
+// x *= y;
+
+4.
+let length = 16; // data type- Number
+let lastName = "Johnson"; // data type- String
+const xs = {firstName: "John", lastName: "Doe"}; // data type- Object
+
+5.
+function myFunction() {
+    alert("Hello World!");
+  }
+  myFunction();
+
+6.
+let person = {
+    name: "John",
+    age: 50,
+  };
+  
+  alert(person.name + " is " + person.age);
+  
+7.
+function myFun() {
+    alert("Raqibul Amin Jaber");
+  }
+
+8.1
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+8.2
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0] = "Ford";
+document.write(Brand)
+
+9.1
+let randomNum = Math.random();
+console.log(randomNum);
+
+9.2
+let maxNumber = Math.max(10, 20);
+console.log(maxNumber);
+
+9.3
+let squRoot = Math.sqrt(9);
+console.log(squRoot);
+
+10.1
+let x = 10;
+let y = 5;
+
+alert(x > y);
+
+10.2
+let Age = 16;
+
+alert(Age < 18 ? "Too young" : "Old enough");
+
+
+
+
